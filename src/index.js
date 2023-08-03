873598324b93a6acfb4597be7031f44991b1abb7
Darya Tselner <duk.darya@gmail.com>
2023-08-03 15:42:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let s = String(Math.abs(n));
+  let i = 0;
+  let result = '';
+  while (i < s.length) {
+    result = s[i] + result;
+    i += 1;
+  };
+
+  return Number(result);
 }
